e58f8b7fe9ecbe545096c98f086487f3efde4486
Bryan Matsuo <bryan.matsuo@gmail.com>
2011-11-03 04:02:28 -0700

    Run Gofix for error fixes. Fix return value shadowing.
    I was overloading and index variable i which was an implicit return
    value. Its meaning was not what I was using it for.

--- spec/exec.go
+++ spec/exec.go
@@ -7,107 +7,107 @@ package spec
  *  Description: <no value>
  */
 import (
-    "reflect"
-    "os"
+	"errors"
+	"reflect"
 )
 
-func (t *SpecTest) exec(fn Function, negated bool, args []interface{}) (pass bool, err os.Error) {
-    if len(args) < 1 {
-        // Serious error
-        t.Errorf("Spec error: Missing Value")
-        return
-    }
-    if negated {
-        defer func() { pass = !pass }()
-    }
-    val := args[0]
+func (t *SpecTest) exec(fn Function, negated bool, args []interface{}) (pass bool, err error) {
+	if len(args) < 1 {
+		// Serious error
+		t.Errorf("Spec error: Missing Value")
+		return
+	}
+	if negated {
+		defer func() { pass = !pass }()
+	}
+	val := args[0]
 
-    if n:= len(args)-1; n < fn.NumArg() {
-        t.Errorf("Spec error: Missing argument")
-        return
-    } else if n > fn.NumArg() {
-        t.Errorf("Spec error: Unexpected arguments %v", args[n:])
-        return
-    }
+	if n := len(args) - 1; n < fn.NumArg() {
+		t.Errorf("Spec error: Missing argument")
+		return
+	} else if n > fn.NumArg() {
+		t.Errorf("Spec error: Unexpected arguments %v", args[n:])
+		return
+	}
 
-    // Handle the Function fn.
-    switch fn {
-    case Equal:
-        pass, err = t.equal(val, args[1])
-    case Satisfy:
-        pass, err = t.satisfy(val, args[1])
-    case HaveError:
-        pass, err = t.haveError(val)
-    }
-    return
+	// Handle the Function fn.
+	switch fn {
+	case Equal:
+		pass, err = t.equal(val, args[1])
+	case Satisfy:
+		pass, err = t.satisfy(val, args[1])
+	case HaveError:
+		pass, err = t.haveError(val)
+	}
+	return
 }
 
-func (t *SpecTest) equal(a, b interface{}) (bool, os.Error) {
-    switch a.(type) {
-    case FnCall:
-        a = a.(FnCall).out[0].Interface()
-    }
-    switch b.(type) {
-    case FnCall:
-        b = b.(FnCall).out[0].Interface()
-    }
+func (t *SpecTest) equal(a, b interface{}) (bool, error) {
+	switch a.(type) {
+	case FnCall:
+		a = a.(FnCall).out[0].Interface()
+	}
+	switch b.(type) {
+	case FnCall:
+		b = b.(FnCall).out[0].Interface()
+	}
 
-    t.doDebug(func() { t.Logf("%#v = %#v", a, b) })
-    return reflect.DeepEqual(a, b), nil
+	t.doDebug(func() { t.Logf("%#v = %#v", a, b) })
+	return reflect.DeepEqual(a, b), nil
 }
 
-func (t *SpecTest) satisfy(x, fn interface{}) (satisfies bool, err os.Error) {
-    t.doDebug(func() { t.Logf("%#v satisfies function %#v", x, fn) })
-    switch x.(type) {
-    case FnCall:
-        x = x.(FnCall).out[0].Interface()
-    default:
-    }
+func (t *SpecTest) satisfy(x, fn interface{}) (satisfies bool, err error) {
+	t.doDebug(func() { t.Logf("%#v satisfies function %#v", x, fn) })
+	switch x.(type) {
+	case FnCall:
+		x = x.(FnCall).out[0].Interface()
+	default:
+	}
 
-    fnval := reflect.ValueOf(fn)
+	fnval := reflect.ValueOf(fn)
 
-    // Check the type of fn (fn(x)bool).
-    if k := fnval.Kind(); k != reflect.Func {
-        return false, os.NewError("Satisfy given non-function")
-    }
-    if typ := fnval.Type(); typ.NumIn() != 1 {
-        return false, os.NewError("Satisfy needs a function of one argument")
-    } else if xtyp := reflect.TypeOf(x); !xtyp.AssignableTo(typ.In(0)) {
-        return false, os.NewError("Satisfy argument type-mismatch")
-    } else if typ.NumOut() != 1 {
-        return false, os.NewError("Satisfy needs a predicate (func(x) bool)")
-    } else if !typ.Out(0).AssignableTo(reflect.TypeOf(satisfies)) {
-        return false, os.NewError("Satisfy output type-mismatch")
-    }
-    fnout := fnval.Call([]reflect.Value{reflect.ValueOf(x)})
-    satisfies = fnout[0].Interface().(bool)
-    return
+	// Check the type of fn (fn(x)bool).
+	if k := fnval.Kind(); k != reflect.Func {
+		return false, errors.New("Satisfy given non-function")
+	}
+	if typ := fnval.Type(); typ.NumIn() != 1 {
+		return false, errors.New("Satisfy needs a function of one argument")
+	} else if xtyp := reflect.TypeOf(x); !xtyp.AssignableTo(typ.In(0)) {
+		return false, errors.New("Satisfy argument type-mismatch")
+	} else if typ.NumOut() != 1 {
+		return false, errors.New("Satisfy needs a predicate (func(x) bool)")
+	} else if !typ.Out(0).AssignableTo(reflect.TypeOf(satisfies)) {
+		return false, errors.New("Satisfy output type-mismatch")
+	}
+	fnout := fnval.Call([]reflect.Value{reflect.ValueOf(x)})
+	satisfies = fnout[0].Interface().(bool)
+	return
 }
 
-func (t *SpecTest) haveError(fn interface{}) (bool, os.Error) {
-    t.doDebug(func() { t.Logf("Function %#v has an error", fn) })
+func (t *SpecTest) haveError(fn interface{}) (bool, error) {
+	t.doDebug(func() { t.Logf("Function %#v has an error", fn) })
 
-    var errval reflect.Value
-    switch fn.(type) {
-    case FnCall:
-        fncall := fn.(FnCall)
-        fntyp := fncall.fn.Type()
-        if !reflect.TypeOf(os.NewError("error")).AssignableTo(fntyp.Out(fntyp.NumOut() - 1)) {
-            return false, os.NewError("HaveError function call's last Value must be os.Error")
-        }
-        errval = fncall.out[len(fncall.out)-1]
-    default:
-        return false, os.NewError("HaveError needs a function call Value")
-    }
+	var errval reflect.Value
+	switch fn.(type) {
+	case FnCall:
+		fncall := fn.(FnCall)
+		fntyp := fncall.fn.Type()
+		if !reflect.TypeOf(errors.New("error")).AssignableTo(fntyp.Out(fntyp.NumOut() - 1)) {
+			return false, errors.New("HaveError function call's last Value must be os.Error")
+		}
+		errval = fncall.out[len(fncall.out)-1]
+	default:
+		return false, errors.New("HaveError needs a function call Value")
+	}
 
-    var fnerr os.Error
-    switch v := errval.Interface(); v.(type) {
-    case nil:
-        fnerr = nil
-    case os.Error:
-        fnerr = v.(os.Error)
-    default:
-        return false, os.NewError("Function call can not have error")
-    }
-    return fnerr != nil, nil
+	var fnerr error
+	switch v := errval.Interface(); v.(type) {
+	case nil:
+		fnerr = nil
+	case error:
+		fnerr = v.(error)
+	default:
+		return false, errors.New("Function call can not have error")
+	}
+	return fnerr != nil, nil
 }
--- spec/parse.go
+++ spec/parse.go
@@ -7,8 +7,8 @@ package spec
  *  Description: <no value>
  */
 import (
-    "reflect"
-    "os"
+	"errors"
+	"reflect"
 )
 //  Syntactic sugar for Spec sequences. See Spec.
 type Sugar uint8
@@ -16,195 +16,196 @@ type Sugar uint8
 const Should Sugar = 0
 
 func (s Sugar) String() string {
-    if s == Should {
-        return "Should"
-    }
-    panic("Bad Sugar")
+	if s == Should {
+		return "Should"
+	}
+	panic("Bad Sugar")
 }
 
 //  Functions over Spec Values that can be used in Spec sequences. See Spec.
 type Function uint
 
 const (
-    Equal Function = iota
-    Satisfy
-    HaveError
-    Not
+	Equal Function = iota
+	Satisfy
+	HaveError
+	Not
 )
 
 var fnNumArg = []int{
-    Equal:     1,
-    Satisfy:   1,
-    HaveError: 0,
-    Not:       1,
+	Equal:     1,
+	Satisfy:   1,
+	HaveError: 0,
+	Not:       1,
 }
 
 var fnString = []string{
-    Equal:     "Equal",
-    Satisfy:   "Satisfy",
-    HaveError: "HaveError",
-    Not:       "Not",
+	Equal:     "Equal",
+	Satisfy:   "Satisfy",
+	HaveError: "HaveError",
+	Not:       "Not",
 }
 
 //  Return the number of arguments taken by Function fn. This is the number
 //  of arguments following the function name in the Spec sequence.
-func (fn Function) NumArg() int    { return fnNumArg[fn] }
+func (fn Function) NumArg() int { return fnNumArg[fn] }
 
 //  Return the Function's name as a string.
 func (fn Function) String() string { return fnString[fn] }
 
 //  A call to a nil-adic function. Output values can be accessed.
 type FnCall struct {
-    fn  reflect.Value
-    out []reflect.Value
+	fn  reflect.Value
+	out []reflect.Value
 }
 
 type token uint
 
 const (
-    tSugar token = iota
-    tFunction
-    tValue
+	tSugar token = iota
+	tFunction
+	tValue
 )
 
 var tokenStr = []string{
-    tSugar: "Sugar",
-    tFunction: "Function",
-    tValue: "Value",
+	tSugar:    "Sugar",
+	tFunction: "Function",
+	tValue:    "Value",
 }
-func (t token) String() string { return tokenStr[t] }
-func (t token) IsSugar() bool { return t == tSugar }
+
+func (t token) String() string   { return tokenStr[t] }
+func (t token) IsSugar() bool    { return t == tSugar }
 func (t token) IsFunction() bool { return t == tFunction }
-func (t token) IsValue() bool { return t == tValue }
+func (t token) IsValue() bool    { return t == tValue }
 
 func tokenOf(v interface{}) (t token) {
-    switch v.(type) {
-    case Function:
-        t = tFunction
-    case Sugar:
-        t = tSugar
-    default:
-        t = tValue
-    }
-    return
+	switch v.(type) {
+	case Function:
+		t = tFunction
+	case Sugar:
+		t = tSugar
+	default:
+		t = tValue
+	}
+	return
 }
 
 func tokenize(sequence []interface{}) []token {
-    tokens := make([]token, len(sequence))
-    for i := range sequence {
-        tokens[i] = tokenOf(sequence[i])
-    }
-    return tokens
+	tokens := make([]token, len(sequence))
+	for i := range sequence {
+		tokens[i] = tokenOf(sequence[i])
+	}
+	return tokens
 }
 
 type kind uint
 
 const (
-    kNative kind = iota
-    kFnCall
+	kNative kind = iota
+	kFnCall
 )
 
 type elem struct {
-    token
-    kind
-    value interface{}
+	token
+	kind
+	value interface{}
 }
 
 type sequence []elem
 
 var (
-    ErrBadSugar = os.NewError("Bad Sugar")
-    ErrBadFunction = os.NewError("Bad Value")
-    ErrMissingSugar = os.NewError("Missing Value")
-    ErrMissingFunction = os.NewError("Missing Value")
-    ErrMissingValue = os.NewError("Missing Value")
-    ErrUnexpectedSugar = os.NewError("Unexpected Sugar")
-    ErrUnexpectedFunction = os.NewError("Unexpected Function")
-    ErrUnexpectedValue = os.NewError("Unexpected Value")
+	ErrBadSugar           = errors.New("Bad Sugar")
+	ErrBadFunction        = errors.New("Bad Value")
+	ErrMissingSugar       = errors.New("Missing Value")
+	ErrMissingFunction    = errors.New("Missing Value")
+	ErrMissingValue       = errors.New("Missing Value")
+	ErrUnexpectedSugar    = errors.New("Unexpected Sugar")
+	ErrUnexpectedFunction = errors.New("Unexpected Function")
+	ErrUnexpectedValue    = errors.New("Unexpected Value")
 )
 
-func (t *SpecTest) scan(values []interface{}) (seq sequence, err os.Error) {
-    if len(values) == 0 {
-        return nil, ErrMissingValue
-    }
+func (t *SpecTest) scan(values []interface{}) (seq sequence, err error) {
+	if len(values) == 0 {
+		return nil, ErrMissingValue
+	}
 
-    seq = make(sequence, len(values))
-    for i, t := range tokenize(values) {
-        seq[i] = elem{t, kNative, values[i]}
-    }
-    return
+	seq = make(sequence, len(values))
+	for i, t := range tokenize(values) {
+		seq[i] = elem{t, kNative, values[i]}
+	}
+	return
 }
 
-func (t *SpecTest) parse(seq sequence) (fn Function, negated bool, args []interface{}, err os.Error) {
-    var (
-        i, k   int          // Index,   Gobbled
-        v1, v2 interface{}  // Object,  Argument
-    )
+func (t *SpecTest) parse(seq sequence) (fn Function, negated bool, args []interface{}, err error) {
+	var (
+		i, k   int         // Index,   Gobbled
+		v1, v2 interface{} // Object,  Argument
+	)
 
-    t.doDebug(func() { t.Log(specString(seq[i:])) })
-    // Parse the object Value to be "spec'ed"
-    k, v1, err = t.parseValue(seq)
-    i += k
-    if err != nil {
-        return
-    }
+	t.doDebug(func() { t.Log(specString(seq[i:])) })
+	// Parse the object Value to be "spec'ed"
+	k, v1, err = t.parseValue(seq)
+	i += k
+	if err != nil {
+		return
+	}
 
-    t.doDebug(func() { t.Log(specString(seq[i:])) })
-    // Look for Should separating value and function.
-    switch seq[i].token {
-    case tSugar:
-        if seq[i].value.(Sugar) != Should {
-            err = ErrBadSugar
-        }
-    case tFunction:
-        err = ErrMissingSugar
-    default:
-        err = ErrUnexpectedValue
-    }
-    i++
+	t.doDebug(func() { t.Log(specString(seq[i:])) })
+	// Look for Should separating value and function.
+	switch seq[i].token {
+	case tSugar:
+		if seq[i].value.(Sugar) != Should {
+			err = ErrBadSugar
+		}
+	case tFunction:
+		err = ErrMissingSugar
+	default:
+		err = ErrUnexpectedValue
+	}
+	i++
 
-    t.doDebug(func() { t.Log(specString(seq[i:])) })
-    // Look for function negation, Not.
-    negated = false
-    switch seq[i].token {
-    case tFunction:
-        if seq[i].value.(Function) == Not {
-            negated = true
-            i++
-        }
-    default:
-    }
+	t.doDebug(func() { t.Log(specString(seq[i:])) })
+	// Look for function negation, Not.
+	negated = false
+	switch seq[i].token {
+	case tFunction:
+		if seq[i].value.(Function) == Not {
+			negated = true
+			i++
+		}
+	default:
+	}
 
-    if negated {
-        t.doDebug(func() { t.Log(specString(seq[i:])) })
-    }
-    // Look for Function.
-    k, fn, err = t.parseFunction(seq[i:])
-    i += k
-    if err != nil {
-        return
-    }
+	if negated {
+		t.doDebug(func() { t.Log(specString(seq[i:])) })
+	}
+	// Look for Function.
+	k, fn, err = t.parseFunction(seq[i:])
+	i += k
+	if err != nil {
+		return
+	}
 
-    args = make([]interface{}, 1, 2)
-    args[0] = v1
-    if fn.NumArg() > 0 {
-        // Look for a Function argument if necessary.
-        if fn.NumArg() > 1 {
-            err = os.NewError("Needs too many arguments")
-            return
-        }
-        t.doDebug(func() { t.Log(specString(seq[i:])) })
-        k, v2, err = t.parseValue(seq[i:])
-        i += k
-        if err != nil {
-            return
-        }
-        if i < len(seq) {
-            err = os.NewError("Excess specification pieces")
-        }
-        args = append(args, v2)
-    }
-    return
+	args = make([]interface{}, 1, 2)
+	args[0] = v1
+	if fn.NumArg() > 0 {
+		// Look for a Function argument if necessary.
+		if fn.NumArg() > 1 {
+			err = errors.New("Needs too many arguments")
+			return
+		}
+		t.doDebug(func() { t.Log(specString(seq[i:])) })
+		k, v2, err = t.parseValue(seq[i:])
+		i += k
+		if err != nil {
+			return
+		}
+		if i < len(seq) {
+			err = errors.New("Excess specification pieces")
+		}
+		args = append(args, v2)
+	}
+	return
 }
 /*
 func (t *SpecTest) parseSpec(specpieces []interface{}) (fn Function, negated bool, args []interface{}, err os.Error) {
@@ -284,118 +285,118 @@ func (t *SpecTest) parseSpec(specpieces []interface{}) (fn Function, negated boo
 }
 */
 
-func (t *SpecTest) parseValue(seq sequence) (i int, v interface{}, err os.Error) {
-    if len(seq) == 0 {
-        panic("empty")
-    }
+func (t *SpecTest) parseValue(seq sequence) (i int, v interface{}, err error) {
+	if len(seq) == 0 {
+		panic("empty")
+	}
 
-    // Gobble pieces of the Value.
-    var valpieces sequence
-    done := false
-    for !done && i < len(seq) {
-        var piece elem = seq[i]
-        switch piece.token {
-        case tSugar:
-            if i == 0 {
-                err = ErrMissingValue
-                return
-            }
-            // Piece Should terminates the value.
-            switch piece.value.(Sugar) {
-            case Should:
-            default:
-                err = ErrBadSugar
-                return
-            }
-            i--
-            done = true
-        case tFunction:
-            err = ErrMissingSugar
-            return
-        default:
-            valpieces = append(valpieces, piece)
-        }
-        i++
-    }
+	// Gobble pieces of the Value.
+	var valpieces sequence
+	done := false
+	for !done && i < len(seq) {
+		var piece elem = seq[i]
+		switch piece.token {
+		case tSugar:
+			if i == 0 {
+				err = ErrMissingValue
+				return
+			}
+			// Piece Should terminates the value.
+			switch piece.value.(Sugar) {
+			case Should:
+			default:
+				err = ErrBadSugar
+				return
+			}
+			i--
+			done = true
+		case tFunction:
+			err = ErrMissingSugar
+			return
+		default:
+			valpieces = append(valpieces, piece)
+		}
+		i++
+	}
 
-    // The object of the Spec Function.
-    v = valpieces[0].value
-    valpieces[0].kind = kNative
+	// The object of the Spec Function.
+	v = valpieces[0].value
+	valpieces[0].kind = kNative
 
-    // Evaluate nil-adic function calls.
-    switch v.(type) {
-    case FnCall:
-    default:
-        fntyp := reflect.TypeOf(v)
-        if k := fntyp.Kind(); k != reflect.Func {
-            break
-        }
-        if fntyp.NumIn() != 0 {
-            break
-        }
-        if fntyp.NumOut() == 0 {
-            err = os.NewError("Value-less function")
-            return
-        }
-        valpieces[0].kind = kFnCall
-        fnval := reflect.ValueOf(v)
-        v = FnCall{fnval, fnval.Call([]reflect.Value{})}
-    }
+	// Evaluate nil-adic function calls.
+	switch v.(type) {
+	case FnCall:
+	default:
+		fntyp := reflect.TypeOf(v)
+		if k := fntyp.Kind(); k != reflect.Func {
+			break
+		}
+		if fntyp.NumIn() != 0 {
+			break
+		}
+		if fntyp.NumOut() == 0 {
+			err = errors.New("Value-less function")
+			return
+		}
+		valpieces[0].kind = kFnCall
+		fnval := reflect.ValueOf(v)
+		v = FnCall{fnval, fnval.Call([]reflect.Value{})}
+	}
 
-    // Return when no indexing values are given.
-    if len(valpieces) <= 1 {
-        return
-    }
+	// Return when no indexing values are given.
+	if len(valpieces) <= 1 {
+		return
+	}
 
-    if len(valpieces) > 2 {
-        err = os.NewError("Too many Value pieces")
-        return
-    }
+	if len(valpieces) > 2 {
+		err = errors.New("Too many Value pieces")
+		return
+	}
 
-    // Create a slice of reflect.Values to index
-    var vals []reflect.Value
-    switch v.(type) {
-    case FnCall:
-        vals = v.(FnCall).out
-    default:
-        // Can't index the Value.
-        err = os.NewError("Too many Value pieces")
-        return
-    }
+	// Create a slice of reflect.Values to index
+	var vals []reflect.Value
+	switch v.(type) {
+	case FnCall:
+		vals = v.(FnCall).out
+	default:
+		// Can't index the Value.
+		err = errors.New("Too many Value pieces")
+		return
+	}
 
-    // Index the Value to create another Value.
-    index := valpieces[1]
-    switch index.value.(type) {
-    case int:
-        i := index.value.(int)
-        if i >= len(vals) {
-            err = os.NewError("Index out of range")
-            return
-        }
-        v = vals[i].Interface()
-    default:
-        err = os.NewError("Missing 'int' Value index")
-    }
-    return
+	// Index the Value to create another Value.
+	index := valpieces[1]
+	switch index.value.(type) {
+	case int:
+		j := index.value.(int)
+		if j >= len(vals) {
+			err = errors.New("Index out of range")
+			return
+		}
+		v = vals[j].Interface()
+	default:
+		err = errors.New("Missing 'int' Value index")
+	}
+	return
 }
 
-func (t *SpecTest) parseFunction(seq sequence) (i int, fn Function, err os.Error) {
-    if len(seq) == 0 {
-        err = os.NewError("empty")
-        return
-    }
+func (t *SpecTest) parseFunction(seq sequence) (i int, fn Function, err error) {
+	if len(seq) == 0 {
+		err = errors.New("empty")
+		return
+	}
 
-    switch piece := seq[0]; piece.token {
-    case tFunction:
-        if fn = piece.value.(Function); fn == Not {
-            // Function Not is handled before.
-            err = os.NewError("Double negative")
-        }
-    case tSugar:
-        err = os.NewError("Unexpected Sugar")
-    default:
-        err = os.NewError("Missing Function")
-    }
-    i++
-    return
+	switch piece := seq[0]; piece.token {
+	case tFunction:
+		if fn = piece.value.(Function); fn == Not {
+			// Function Not is handled before.
+			err = errors.New("Double negative")
+		}
+	case tSugar:
+		err = errors.New("Unexpected Sugar")
+	default:
+		err = errors.New("Missing Function")
+	}
+	i++
+	return
 }
